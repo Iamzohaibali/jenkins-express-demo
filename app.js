c254c7b75bd878f70c2da60ab0475a716c654d1c
Iamzohaibali <j36740062@gmail.com>
2026-06-25 14:14:15 +0500

    second version

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-const express = require("express");
-
+const express = require('express');
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello from Jenkins!");
+app.get('/', (req, res) => {
+  res.send('Version 2 🚀');
 });
 
 const PORT = 3000;
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
